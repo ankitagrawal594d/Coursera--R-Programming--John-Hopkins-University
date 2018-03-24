4e77beb88745bbc0be694d3409e6239b39d4c65f
ankitagrawal594d <37749752+ankitagrawal594d@users.noreply.github.com>
2018-03-25 00:36:33 +0530

    Update pollutantmean.R
    Programming Assignment 1: Part 1 (Air Pollution)

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,1 +1,12 @@
+pollutantmean <- function(directory, pollutant, id = 1:332)
+{
+  file_names<-list.files(directory,full.names=TRUE)
+  pollutantdata <- vector("numeric")
+  for (i in id)
+  {
+    data<-read.csv(file_names[i])
+    pollutantdata<-c(pollutantdata,data[,pollutant])
+  }
+  mean(pollutantdata,na.rm=TRUE)
+}
 
